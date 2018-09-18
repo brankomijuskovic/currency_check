0360354851a2c9a0d5aa9f6daa05a3d11e2ec056
root <root@branko-docker.localdomain>
2018-09-18 19:06:35 +0000

    Added sllep_time as a variable

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ import os
 
 api_url = 'http://data.fixer.io/api/latest?access_key='
 api_key = os.environ['API_KEY']
+sleep_time = os.environ['SLEEP']
 
 response = requests.request("GET", api_url + api_key)
 result = 1 / float(json.loads(response.text)['rates']['CHF'])
 
 while True:
     print(round(result, 5))
-    time.sleep(3600)
+    time.sleep(sleep_time)
 
