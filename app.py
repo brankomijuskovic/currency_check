35d6bcff9f77b2c1e154d7b235ae0248cad10d77
Branko <brankomijuskovic@gmail.com>
2018-10-07 20:17:18 +0200

    Added influxdb as data store

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ import json
 import requests
 import time
 import os
+from influxdb import InfluxDBClient
 
 api_url = 'http://data.fixer.io/api/latest?access_key='
 api_key = os.environ['API_KEY']
 sleep_time = os.environ['SLEEP']
+influxdb_host = os.environ['INFLUXDB_HOST']
+
 
 response = requests.request("GET", api_url + api_key)
 result = 1 / float(json.loads(response.text)['rates']['CHF'])
 
-while True:
-    print(round(result, 5))
-    time.sleep(int(sleep_time))
+client = InfluxDBClient(influxdb_host, 8086, 'root', 'root', 'chf_data')
+client.create_database('chf_data')
 
+
+while True:
+    client.write_points([{"measurement": "CHF", "fields": {"value": round(result, 5)}}])
+    time.sleep(int(sleep_time))
